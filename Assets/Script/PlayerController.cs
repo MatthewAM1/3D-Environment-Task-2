0f5b790d4f41e0b0a2c7c4807c6d9a696e5663d0
MatthewAM1 <matthewalden.ma@gmail.com>
2024-06-29 10:07:23 +0700

    Simple Multiplayer Game

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,15 +6,34 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float playerSpeed = 5f;
     [SerializeField] private float rotationSpeed = 100f;
+    [SerializeField] private string inputID;
 
     private float horizontalInput;
     private float verticalInput;
 
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX;
+    [SerializeField] private float minZ;
+    [SerializeField] private float maxZ;
+
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        verticalInput = Input.GetAxis("Vertical" + inputID);
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Teleport();
+        }
+    }
+
+    private void Teleport()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomZ = Random.Range(minZ, maxZ);
+        Vector3 newPosition = new Vector3(randomX, transform.position.y, randomZ);
+        transform.position = newPosition;
     }
 
     private void FixedUpdate()
